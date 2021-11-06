270ac17b374238001aec4019f5599fe788d2fc07
Aleksandra Podhurska <a.podhurska@gmail.com>
2021-11-06 20:02:48 +0200

    removed unwanted data

--- server/servise/mail-servise.js
+++ server/servise/mail-servise.js
@@ -1,33 +1,32 @@
-require('dotenv').config();
-const nodemailer = require('nodemailer');
+require("dotenv").config();
+const nodemailer = require("nodemailer");
 
 class mailServise {
-    constructor() {
-        this.transporter = nodemailer.createTransport({
-            host: "***REMOVED***",
-            port: ***REMOVED***,
-            secure: false,
-            auth: {
-                user: "***REMOVED***",
-                pass: "***REMOVED***"
-            }
-        })
-    }
-    async sendActivationLink(to, link) {
-        await this.transporter.sendMail({
-            from: process.env.SMTP_USER,
-            to,
-            subject: `Account activation ${process.env.API_URL}`,
-            text: "",
-            html: `
+	constructor() {
+		this.transporter = nodemailer.createTransport({
+			host: process.env.SMTP_HOST,
+			port: process.env.SMTP_PORT,
+			secure: false,
+			auth: {
+				user: process.env.SMTP_USER,
+				pass: process.env.SMTP_PASSWORD,
+			},
+		});
+	}
+	async sendActivationLink(to, link) {
+		await this.transporter.sendMail({
+			from: process.env.SMTP_USER,
+			to,
+			subject: `Account activation ${process.env.API_URL}`,
+			text: "",
+			html: `
                 <div>
                     <h1>Follow the link to activate your accont: </h1>
                     <a href="${link}">${link}</a>
                 </div>
-            `
-        })
-
-    }
+            `,
+		});
+	}
 }
 
 module.exports = new mailServise();
